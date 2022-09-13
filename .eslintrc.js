80a02982e3b5eeb943448eceb15b6f319a2b4530
ForFriendsFF <113406473+ForFriendsFF@users.noreply.github.com>
2022-09-13 11:14:45 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,31 @@ module.exports = {
       },
     ],
   },
-  ignorePatterns: ['*.md'],
+ export default defineConfig({
+  plugins: [createVuePlugin(/* options */), graphql()],
+  test: {
+    include: ['tests/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
+    globals: true,
+    environment: 'jsdom',
+    alias: [{ find: /^vue$/, replacement: 'vue/dist/vue.runtime.common.js' }],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './'),
+      '~': resolve(__dirname, './'),
+    },
+  },
+})
+ignorePatterns: ['*.md'],
   overrides: [
     {
       files: ['layouts/**/*.{js,ts,vue}', 'pages/**/*.vue'],
